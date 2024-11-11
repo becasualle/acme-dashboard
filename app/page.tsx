97a8867a0750e0dd0eaea49d2315d8793203788c
Nelyubin Mark <mark.nelubin@gmail.com>
2024-11-11 09:48:56 +0300

    added optimized images

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,15 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
+/*
+  The <Image> Component is an extension of the HTML <img> tag, and comes with automatic image optimization, such as:
+
+  1. Preventing layout shift automatically when images are loading.
+  2. Resizing images to avoid shipping large images to devices with a smaller viewport.
+  3. Lazy loading images by default (images load as they enter the viewport).
+  4. Serving images in modern formats, like WebP and AVIF, when the browser supports it.
+*/
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -29,7 +38,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
