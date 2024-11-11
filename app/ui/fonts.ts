00d88d067bf892520413c0f1e0e7f04d1f9de794
Nelyubin Mark <mark.nelubin@gmail.com>
2024-11-11 09:34:02 +0300

    added comment how next's font optimization works

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,9 @@
+/*
+  Next.js automatically optimizes fonts in the application when you use the `next/font` module.
+  It downloads font files at build time and hosts them with your other static assets.
+  This means when a user visits your application, there are no additional network requests
+  for fonts which would impact performance.
+*/
 import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
